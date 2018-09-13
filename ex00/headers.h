2dca3c03cfa5615df1fb7808b999bfda150a6fc5
Nelson andres Diaz acero <ndiaz-ac@e1z1r5p47.42.us.org>
2018-09-12 17:24:45 -0700

    correction header file

--- ex00/headers.h
+++ ex00/headers.h
@@ -1,14 +1,30 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   headers.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ndiaz-ac <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/10 13:53:37 by ndiaz-ac          #+#    #+#             */
+/*   Updated: 2018/09/10 14:21:13 by ndiaz-ac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADERS_H
+# define HEADERS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
 int		ft_putchar(char c);
 void	ft_putstr(char *str);
 int		ft_strlen(char *str);
 void	ft_putnbr(int nb);
 int		ft_atoi(char **str);
-int 	do_op(int a, char o, int b);
+int		do_op(int a, char o, int b);
 int		sum_sub(char **str);
 int		ft_atoi(char **str);
 int		first_op(char **str);
-int		sum_sub(char **str);
 
+#endif
